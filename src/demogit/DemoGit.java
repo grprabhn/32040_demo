41af3116272066d83f1ffc7667d3757e9da37632
grprabhn <133983577+grprabhn@users.noreply.github.com>
2023-05-25 09:59:17 -0400

    Update DemoGit.java
    a b added

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -16,6 +16,9 @@ public class DemoGit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("noor");
+        int a;
+        int b;
+        int c= a+b;
     }
     
 }
